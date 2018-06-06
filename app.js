bc7e80c6c11eaa6a65cbe213fbf198a794bc6dba
Simon Arazi <s.arazi770@gmail.com>
2018-06-07 00:08:31 +0300

    v2

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
 
-    $('#colContain').height($(window).height());
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    particlesJS.load('particles-js', 'particlesjs-config.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
 
     let rows = $('.rowed');
     let count = 1;
     $.each(rows, function(key, value){
         for (let i = 1; i<= 10; i++) {
-            let squareHTML = $('<div class="square" id="'+count+'"><a href="#">'+count+'</a></div>');
+            let squareHTML = $('<div class="square gradient-border" id="'+count+'"><a class="" href="#">'+count+'</a></div>');
             $(this).append(squareHTML);
             count++;
         }
@@ -16,10 +19,16 @@ $(document).ready(function(){
        e.preventDefault();
        $(this).css({
            'background-color': 'rgba(0,0,0,0)',
+           'background-image': 'none !important',
            'color': 'rgba(0,0,0,0)',
            'border': 'inherit !important'
 
        });
+       $(this).addClass('clicked');
+       $(this).parent().addClass('clicked');
+    });
+
+    $('#yike').click(function(e){
         $('#todaRaba').modal();
     });
 
