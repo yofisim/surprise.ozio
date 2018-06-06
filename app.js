cbf3e6ebbc7aadb0ab29cbb338e06de843313b27
Simon <s.arazi770@gmail.com>
2018-06-06 14:45:40 +0300

    V1

--- app.js
+++ app.js
@@ -1,43 +1,26 @@
 $(document).ready(function(){
-    //Set Image to Full Height of Window VP
-    const windowHeight = $( window ).height();
-    let tnkImg = $('#tnkImg');
-    tnkImg.height(windowHeight);
 
+    $('#colContain').height($(window).height());
 
-    //Create the squares
-    //Get the exact measurements
-    // Iterate over the whole Image
+    let rows = $('.rowed');
+    let count = 1;
+    $.each(rows, function(key, value){
+        for (let i = 1; i<= 10; i++) {
+            let squareHTML = $('<div class="square" id="'+count+'"><a href="#">'+count+'</a></div>');
+            $(this).append(squareHTML);
+            count++;
+        }
+    });
 
-    let tnkProp = {
-        'height': tnkImg.height(),
-        'width': tnkImg.width(),
-    };
+    $('.square a').click(function(e){
+       e.preventDefault();
+       $(this).css({
+           'background-color': 'rgba(0,0,0,0)',
+           'color': 'rgba(0,0,0,0)',
+           'border': 'inherit !important'
 
-    const squareWidth = tnkProp.width / 12;
-    const squareHeight = tnkProp.height / 16.666667;
-
-    // var spaceSquares = function ( n, sWidth, imgWidth ) {
-    //     let leftSpace = sWidth * (n-1);
-    //     if (leftSpace < imgWidth) {
-    //         $('#'+n+'').css({
-    //             'left': 'calc(15px + '+leftSpace+'px)'
-    //         });
-    //     }
-    // }
-
-
-    for (let i = 1; i<= 200; i++) {
-        let squareHTML = $('<div class="square" id="'+i+'"><a href="#">'+i+'</a></div>');
-        squareHTML.width(squareWidth);
-        squareHTML.height(squareHeight);
-
-
-
-    }
-
-
+       });
+        $('#todaRaba').modal();
+    });
 
 });
-
-// $('#colContain').append(squareHTML);
